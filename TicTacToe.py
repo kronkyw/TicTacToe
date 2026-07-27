5b6449f104593f68653284c38414603e9e15ffa0
Kronky_Wonky <damipavlovic75@gmail.com>
2026-07-27 20:50:28 +0000

    modified:   TicTacToe.py 	modified:   __pycache__/TicTacToe.cpython-314.pyc 	deleted:    main.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,24 +1,38 @@
 #A module for my TicTacToe game
 
 import tkinter as tk
+from tkinter import ttk
 
 BOARD_LEN = 3
 
-Board = [['X'] * BOARD_LEN] * BOARD_LEN
-
-
 window = tk.Tk()
 window.config(bg="skyblue")
-tk.title(window, text="TicTacToe").pack()
+window.title("TicTacToe")
+
+current_player = 'X'
+
+Board = [[''] * BOARD_LEN] * BOARD_LEN
 
 for i in range (BOARD_LEN):
-    for j in range (BOARD_LEN):
-        tk.Button
-        (
-            window,
-            text = f"Cell ({i}, {j})",
-            width = 10
-            height = 10
-        ).grid(row=i, column=j)
+    window.rowconfigure(i, weight = 1)
+    window.columnconfigure(i, weight = 1)
+
+def create_board():
+    for i in range (BOARD_LEN):
+            for j in range (BOARD_LEN):
+                Board[i][j] = ttk.Button(window, text = ' ', command = lambda: board_clicked(i, j))
+                Board[i][j].grid(column = j, row = i)
+
+def switch_player(current_player):
+    if current_player == 'X':
+        current_player = 'O'
+    elif current_player == 'O':
+        current_player = 'X'
+
+def board_clicked(x, y):
+    switch_player(current_player)
+    print(x, y)
+
+create_board()
 
 window.mainloop()
