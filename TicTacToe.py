cdab18e72cd9cad9f6a790f7884110bf9dcdabb2
Kronky_Wonky <damipavlovic75@gmail.com>
2026-07-27 21:44:01 +0000

    Finally made it somewhat work

--- TicTacToe.py
+++ TicTacToe.py
@@ -20,18 +20,18 @@ for i in range (BOARD_LEN):
 def create_board():
     for i in range (BOARD_LEN):
             for j in range (BOARD_LEN):
-                Board[i][j] = ttk.Button(window, text = ' ', command = lambda: board_clicked(i, j))
-                Board[i][j].grid(column = j, row = i)
+                Board[i][j] = ttk.Button(window, text = '', command = lambda i=i, j=j: board_clicked(i, j))
+                Board[i][j].grid(column = i, row = j)
 
-def switch_player(current_player):
+def board_clicked(x, y):
+    global current_player
+    print(x, y, current_player)
+    Board[x][y] = ttk.Button(window, text = current_player)
+    Board[x][y].grid(column = x, row = y)
     if current_player == 'X':
         current_player = 'O'
     elif current_player == 'O':
         current_player = 'X'
-
-def board_clicked(x, y):
-    switch_player(current_player)
-    print(x, y)
 
 create_board()
 
