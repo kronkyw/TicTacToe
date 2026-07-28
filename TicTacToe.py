62aa06002b356e3a5e4707ee1f9c5070fc62749d
Kronky_Wonky <damipavlovic75@gmail.com>
2026-07-28 21:22:29 +0000

    Built the project and made finishing touches

--- TicTacToe.py
+++ TicTacToe.py
@@ -14,6 +14,7 @@ turns = 0
 
 restart = ttk.Button(window, text = "Restart", command = lambda: create_board())
 label = ttk.Label(window)
+label.pack()
 
 current_player = 'X'
 
@@ -35,7 +36,7 @@ for i in range (BOARD_LEN):
 def create_board():
     frame.pack()
     restart.pack_forget()
-    label.pack_forget()
+    label.config(text = ("Current player: ", current_player))
     for i in range (BOARD_LEN):
             for j in range (BOARD_LEN):
                 Board_info[i][j] = ''
@@ -51,78 +52,58 @@ def switch_player():
         return 'X'
 
 def check_for_win():
-    X = 0
-    O = 0
+    global X, O
+    X = 0; O = 0
     for i in range(BOARD_LEN):
         for j in range(BOARD_LEN):
-            if Board_info[i][j] == 'X' and O == 0:
-                X += 1
-            elif Board_info[i][j] == 'O' and X == 0:
-                O += 1
-            else:
+            if check(i, j) == False: 
                 break
-            if X == BOARD_LEN: game_finished('X')
-            if O == BOARD_LEN: game_finished('O')
-        X = 0
-        O = 0
+        X = 0; O = 0
+    X = 0; O = 0
     for i in range(BOARD_LEN):
         for j in range(BOARD_LEN):
-            if Board_info[j][i] == 'X' and O == 0:
-                X += 1
-            elif Board_info[j][i] == 'O' and X == 0:
-                O += 1
-            else:
+            if check(j, i) == False: 
                 break
-            if X == BOARD_LEN: game_finished('X')
-            if O == BOARD_LEN: game_finished('O')
-        X = 0
-        O = 0
+        X = 0; O = 0
+    X = 0; O = 0
     for i in range(BOARD_LEN):
-        if Board_info[i][i] == 'X' and O == 0:
-            X += 1
-        elif Board_info[i][i] == 'O' and X == 0:
-            O += 1
-        else:
+        if check(i, i) == False: 
             break
-        if X == BOARD_LEN: game_finished('X')
-        if O == BOARD_LEN: game_finished('O')
+        X = 0; O = 0
+    X = 0; O = 0
     for i in range(BOARD_LEN):
-        if Board_info[BOARD_LEN - 1 - i][i] == 'X' and O == 0:
-            X += 1
-        elif Board_info[BOARD_LEN - 1 - i][i] == 'O' and X == 0:
-            O += 1
-        else:
+        if check(BOARD_LEN - 1 - i, i) == False: 
             break
-        if X == BOARD_LEN: game_finished('X')
-        if O == BOARD_LEN: game_finished('O')
     return False
+
+def check(i, j):
+    global X, O
+    if Board_info[i][j] == 'X' and O == 0: X += 1
+    elif Board_info[i][j] == 'O' and X == 0: O += 1
+    else: return False
+    if X == BOARD_LEN: game_finished('X')
+    if O == BOARD_LEN: game_finished('O')
 
 # Activates when someone clicks one of the buttons on the board
 def button_clicked(x, y):
-    global current_player
-    global turns
+    global current_player, turns
     Board[x][y] = ttk.Button(frame, text = current_player)
     Board[x][y].grid(column = x, row = y)
     Board_info[y][x] = current_player
-    print(x, y, current_player, turns)
-    print(Board_info[0])
-    print(Board_info[1])
-    print(Board_info[2])
     current_player = switch_player()
+    label.config(text = ("Current player: ", current_player))
     turns += 1
     check_for_win()
     if turns == BOARD_LEN * BOARD_LEN and check_for_win() == False:
         game_finished('D')
 
 def game_finished(winner):
-    global current_player
-    global turns
+    global current_player, turns
     turns = 0
     current_player = 'X'
     label.config(text = ("Winner: ", winner))
     frame.pack_forget()
     restart.pack()
-    label.pack()
 
 create_board()
 
